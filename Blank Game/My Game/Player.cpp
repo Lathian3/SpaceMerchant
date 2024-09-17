155e94d107176d46aced489a51c0c196e0f2e27f
Lathian3 <143963888+Lathian3@users.noreply.github.com>
2024-09-16 19:30:47 -0500

    Add files via upload

---
+++ Blank Game/My Game/Player.cpp
@@ -0,0 +1,9 @@
+#include "Player.h"
+
+void Player::move() {
+
+}
+
+void Player::draw() {
+
+}
---
+++ Blank Game/My Game/Player.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "BaseObject.h"
+#include "SpriteRenderer.h"
+#include "GameDefines.h"
+
+
+class Player :
+    LBaseObject
+{
+private:
+    
+public:
+    void move();
+    void draw();
+};
+
